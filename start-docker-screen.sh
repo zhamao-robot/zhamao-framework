d601f3314cea76edb5036675133d10e1899ee085
Whale <jesse2061@outlook.com>
2018-12-29 12:16:35 +0800

    Update start-docker-screen.sh

--- start-docker-screen.sh
+++ start-docker-screen.sh
@@ -6,7 +6,7 @@ if [[ "$cq" = "" ]]; then
 fi
 sleep 1s
 
-screen -x -S cq -p 0 -X stuff "sudo docker run -it --rm --name cqbot -v "$(pwd)"/cqbot/:/root/ jesse2061/cqbot-swoole"
+screen -x -S cq -p 0 -X stuff "sudo docker run -it --rm --net=host --name cqbot -v "$(pwd)"/cqbot/:/root/ jesse2061/cqbot-swoole"
 screen -x -S cq -p 0 -X stuff "\n"
 screen -x -S cq -p 0 -X stuff "php start.php"
 screen -x -S cq -p 0 -X stuff "\n"
