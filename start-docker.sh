a8934ece051c8bb4e631be3c1f105460456c7d76
Whale <jesse2061@outlook.com>
2018-12-29 12:16:13 +0800

    Update start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -it --rm --name cqbot -v $(pwd)/cqbot/:/root/ jesse2061/cqbot-swoole
+sudo docker run -it --rm --net=host --name cqbot -v $(pwd)/cqbot/:/root/ jesse2061/cqbot-swoole
