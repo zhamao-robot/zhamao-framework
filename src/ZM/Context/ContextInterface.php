e3ffeec78293a11cbb1bf5702c6ee5e4d3b0bf30
whale <jesse2061@outlook.com>
2020-04-26 15:08:09 +0800

    update for IDE support

--- src/ZM/Context/ContextInterface.php
+++ src/ZM/Context/ContextInterface.php
@@ -4,19 +4,30 @@
 namespace ZM\Context;
 
 
+use Swoole\Http\Request;
+use Swoole\WebSocket\Frame;
+use Swoole\WebSocket\Server;
+use ZM\Http\Response;
+
 interface ContextInterface
 {
     public function __construct($param, $cid);
 
+    /** @return Server */
     public function getServer();
 
+    /** @return Frame */
     public function getFrame();
 
+    /** @return mixed */
     public function getData();
 
+    /** @return int */
     public function getCid();
 
+    /** @return Response */
     public function getResponse();
 
+    /** @return Request */
     public function getRequest();
 }
