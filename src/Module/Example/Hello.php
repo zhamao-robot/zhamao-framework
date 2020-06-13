500ec4707ee9b3623d622a81858fd1032ad15752
whale <jesse2061@outlook.com>
2020-06-13 17:08:13 +0800

    update Hello default module

--- src/Module/Example/Hello.php
+++ src/Module/Example/Hello.php
@@ -29,6 +29,15 @@ class Hello
     }
 
     /**
+     * 在机器人连接后向终端输出信息
+     * @SwooleEventAt("close",rule="connectType:qq")
+     */
+    public function onDisconnect() {
+        $conn = ctx()->getConnection();
+        Console::info("机器人 " . $conn->getQQ() . " 已断开连接！");
+    }
+
+    /**
      * 向机器人发送"你好"，即可回复这句话
      * @CQCommand(match="你好",alias={"你好啊","你是谁"})
      */
