dbfe2c9c1779812e65d2e224226b416c7301c085
jerry <jesse2061@outlook.com>
2020-11-23 00:24:33 +0800

    update documents

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -3,7 +3,7 @@ hljs.initHighlighting()
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?3d2aafad13637de271f50351e873b776";
-  var s = document.getElementsByTagName("script")[0]; 
+  hm.src = "https://hm.baidu.com/hm.js?f0f276cefa10aa31a20ae3815a50b795";
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
