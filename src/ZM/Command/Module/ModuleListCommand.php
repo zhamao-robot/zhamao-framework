a879f11f1a381f88089a817ac9fd33506b366ef7
crazywhalecc <jesse2061@outlook.com>
2022-04-02 19:15:03 +0800

    refactor module command to phpstan-level-1

--- src/ZM/Command/Module/ModuleListCommand.php
+++ src/ZM/Command/Module/ModuleListCommand.php
@@ -4,47 +4,31 @@ declare(strict_types=1);
 
 namespace ZM\Command\Module;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use ZM\Config\ZMConfig;
 use ZM\Console\Console;
+use ZM\Exception\ZMException;
 use ZM\Utils\DataProvider;
 use ZM\Utils\Manager\ModuleManager;
 
-class ModuleListCommand extends Command
+class ModuleListCommand extends ModuleCommand
 {
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'module:list';
 
     protected function configure()
     {
+        parent::configure();
         $this->setDescription('查看所有模块信息');
         $this->setHelp('此功能将会把炸毛框架的模块列举出来。');
-        // ...
     }
 
+    /**
+     * @throws ZMException
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        ZMConfig::setDirectory(DataProvider::getSourceRootDir() . '/config');
-        ZMConfig::setEnv($args['env'] ?? '');
-        if (ZMConfig::get('global') === false) {
-            exit(zm_internal_errcode('E00007') . 'Global config load failed: ' . ZMConfig::$last_error . "\nPlease init first!\nSee: https://github.com/zhamao-robot/zhamao-framework/issues/37\n");
-        }
-
-        // 定义常量
-        /** @noinspection PhpIncludeInspection */
-        include_once DataProvider::getFrameworkRootDir() . '/src/ZM/global_defines.php';
-
-        Console::init(
-            ZMConfig::get('global', 'info_level') ?? 2,
-            null,
-            $args['log-theme'] ?? 'default',
-            ($o = ZMConfig::get('console_color')) === false ? [] : $o
-        );
-
-        $timezone = ZMConfig::get('global', 'timezone') ?? 'Asia/Shanghai';
-        date_default_timezone_set($timezone);
+        parent::execute($input, $output);
 
         $list = ModuleManager::getConfiguredModules();
 
--- src/ZM/Command/Module/ModuleUnpackCommand.php
+++ src/ZM/Command/Module/ModuleUnpackCommand.php
@@ -4,56 +4,32 @@ declare(strict_types=1);
 
 namespace ZM\Command\Module;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use ZM\Config\ZMConfig;
 use ZM\Console\Console;
-use ZM\Utils\DataProvider;
 use ZM\Utils\Manager\ModuleManager;
 
-class ModuleUnpackCommand extends Command
+class ModuleUnpackCommand extends ModuleCommand
 {
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'module:unpack';
 
     protected function configure()
     {
-        $this->setDefinition([
-            new InputArgument('module-name', InputArgument::REQUIRED),
-            new InputOption('overwrite-light-cache', null, null, '覆盖现有的LightCache项目'),
-            new InputOption('overwrite-zm-data', null, null, '覆盖现有的zm_data文件'),
-            new InputOption('overwrite-source', null, null, '覆盖现有的源码文件'),
-            new InputOption('ignore-depends', null, null, '解包时忽略检查依赖'),
-        ]);
+        parent::configure();
+        $this->addOption('overwrite-light-cache', null, null, '覆盖现有的LightCache项目');
+        $this->addOption('overwrite-zm-data', null, null, '覆盖现有的zm_data文件');
+        $this->addOption('overwrite-source', null, null, '覆盖现有的源码文件');
+        $this->addOption('ignore-depends', null, null, '解包时忽略检查依赖');
+        $this->addArgument('module-name', InputArgument::REQUIRED, '模块名称');
         $this->setDescription('解包一个phar模块到src目录');
         $this->setHelp('此功能将phar格式的模块包解包到src目录下。');
-        // ...
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        ZMConfig::setDirectory(DataProvider::getSourceRootDir() . '/config');
-        ZMConfig::setEnv($args['env'] ?? '');
-        if (ZMConfig::get('global') === false) {
-            exit(zm_internal_errcode('E00007') . 'Global config load failed: ' . ZMConfig::$last_error . "\nPlease init first!\nSee: https://github.com/zhamao-robot/zhamao-framework/issues/37\n");
-        }
-
-        // 定义常量
-        /** @noinspection PhpIncludeInspection */
-        include_once DataProvider::getFrameworkRootDir() . '/src/ZM/global_defines.php';
-
-        Console::init(
-            ZMConfig::get('global', 'info_level') ?? 4,
-            null,
-            $args['log-theme'] ?? 'default',
-            ($o = ZMConfig::get('console_color')) === false ? [] : $o
-        );
-
-        $timezone = ZMConfig::get('global', 'timezone') ?? 'Asia/Shanghai';
-        date_default_timezone_set($timezone);
+        parent::execute($input, $output);
 
         $list = ModuleManager::getPackedModules();
         if (!isset($list[$input->getArgument('module-name')])) {
