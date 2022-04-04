9f7ebceeb946b939dbb601db18513a6c9a11bd43
sunxyw <xy2496419818@gmail.com>
2022-04-05 00:24:37 +0800

    add phpdoc to ZMBuf

--- src/ZM/Store/ZMBuf.php
+++ src/ZM/Store/ZMBuf.php
@@ -10,13 +10,39 @@ declare(strict_types=1);
 
 namespace ZM\Store;
 
+use ZM\Context\ContextInterface;
+
 class ZMBuf
 {
+    /**
+     * 注册的事件
+     *
+     * @deprecated 不再使用
+     *
+     * @var array
+     */
     public static $events = [];
 
+    /**
+     * 全局单例容器
+     *
+     * @var array
+     */
     public static $instance = [];
 
+    /**
+     * 上下文容器
+     *
+     * @var array<int, ContextInterface>
+     */
     public static $context_class = [];
 
+    /**
+     * 终端输入流？
+     *
+     * 目前等用于 STDIN
+     *
+     * @var resource
+     */
     public static $terminal;
 }
