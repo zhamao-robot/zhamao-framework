81d4558b2f079a16d1124f034b0ba3706b602432
sunxyw <git@sunxyw.xyz>
2022-05-22 20:56:05 +0800

    add master support for logger

--- config/logging.php
+++ config/logging.php
@@ -9,10 +9,11 @@ use ZM\Logger\ConsoleLogger;
 return [
     'level' => LogLevel::DEBUG,
     'logger' => static function (): LoggerInterface {
-        $worker_id = app('worker_id');
+        // 在 Master 中，worker_id 将不存在
+        $worker_id = app()->has('worker_id') ? '#' . app('worker_id') : 'Master';
 
         $logger = new ConsoleLogger(zm_config('logging.level'));
-        $logger::$format = "[%date%] [%level%] [#{$worker_id}] %body%";
+        $logger::$format = "[%date%] [%level%] [{$worker_id}] %body%";
         $logger::$date_format = 'Y-m-d H:i:s';
         // 如果你喜欢旧版的日志格式，请取消下行注释
 //        $logger::$date_format = 'm-d H:i:s';
