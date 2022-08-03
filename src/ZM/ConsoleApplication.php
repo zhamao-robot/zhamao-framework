b94bbbca20bd0c07a6e142bf32c4332af0549f10
crazywhalecc <jesse2061@outlook.com>
2022-08-03 17:08:22 +0800

    increment build number (build 476, 2.8.2)

--- src/ZM/ConsoleApplication.php
+++ src/ZM/ConsoleApplication.php
@@ -28,9 +28,9 @@ use ZM\Exception\InitException;
 
 class ConsoleApplication extends Application
 {
-    public const VERSION_ID = 475;
+    public const VERSION_ID = 476;
 
-    public const VERSION = '2.8.1';
+    public const VERSION = '2.8.2';
 
     private static $obj;
 
