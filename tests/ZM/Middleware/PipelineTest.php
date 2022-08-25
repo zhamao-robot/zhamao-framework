529dfe09da74ad97464609ad46dca36db0d4517f
sunxyw <git@sunxyw.xyz>
2022-08-25 15:20:29 +0800

    introduce pest

--- tests/ZM/Middleware/PipelineTest.php
+++ tests/ZM/Middleware/PipelineTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Tests\ZM\Middleware;
 
 use PHPUnit\Framework\TestCase;
-use ZM\Logger\ConsoleLogger;
 use ZM\Middleware\Pipeline;
 use ZM\Middleware\TimerMiddleware;
 
@@ -14,16 +13,6 @@ use ZM\Middleware\TimerMiddleware;
  */
 class PipelineTest extends TestCase
 {
-    public function setUp(): void
-    {
-        ob_logger_register(new ConsoleLogger('debug'));
-    }
-
-    public function tearDown(): void
-    {
-        ob_logger_register(new ConsoleLogger('error'));
-    }
-
     public function testPipeline()
     {
         $pipe = new Pipeline();
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,8 @@
 
 declare(strict_types=1);
 
+use ZM\Logger\ConsoleLogger;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+ob_logger_register(new ConsoleLogger('error'));
