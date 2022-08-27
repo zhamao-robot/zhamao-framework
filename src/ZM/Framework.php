1a9680c09a91aac768173af111d7ed4e5b315d81
sunxyw <sunxyw@users.noreply.github.com>
2022-08-27 08:26:49 +0000

    increment build number (build 620)

--- src/ZM/Framework.php
+++ src/ZM/Framework.php
@@ -41,7 +41,7 @@ class Framework
     use Singleton;
 
     /** @var int 版本ID */
-    public const VERSION_ID = 619;
+    public const VERSION_ID = 620;
 
     /** @var string 版本名称 */
     public const VERSION = '3.0.0-alpha3';
