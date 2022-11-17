8199e91ce07b4ebdae42ddd5863fd5bca81c72c0
sunxyw <git@sunxyw.xyz>
2022-11-17 21:11:11 +0800

    remove todo from InterruptException

--- src/ZM/Exception/InterruptException.php
+++ src/ZM/Exception/InterruptException.php
@@ -10,7 +10,6 @@ class InterruptException extends ZMException
 
     public function __construct($return_var = null, $message = '', $code = 0, \Throwable $previous = null)
     {
-        // TODO: change this to a better error message
         parent::__construct($message, '', $code, $previous);
         $this->return_var = $return_var;
     }
