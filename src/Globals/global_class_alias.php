6290d8bb236726b19a53c02c047314840ebb87a2
crazywhalecc <jesse2061@outlook.com>
2023-01-06 16:08:25 +0800

    update class alias

--- src/Globals/global_class_alias.php
+++ src/Globals/global_class_alias.php
@@ -3,17 +3,25 @@
 declare(strict_types=1);
 
 class_alias(\ZM\Annotation\Framework\BindEvent::class, 'BindEvent');
+class_alias(\ZM\Annotation\Framework\Cron::class, 'Cron');
 class_alias(\ZM\Annotation\Framework\Init::class, 'Init');
 class_alias(\ZM\Annotation\Framework\Setup::class, 'Setup');
+
 class_alias(\ZM\Annotation\Http\Controller::class, 'Controller');
 class_alias(\ZM\Annotation\Http\Route::class, 'Route');
+
 class_alias(\ZM\Annotation\Middleware\Middleware::class, 'Middleware');
+
+class_alias(\ZM\Annotation\OneBot\BotAction::class, 'BotAction');
+class_alias(\ZM\Annotation\OneBot\BotActionResponse::class, 'BotActionResponse');
 class_alias(\ZM\Annotation\OneBot\BotCommand::class, 'BotCommand');
 class_alias(\ZM\Annotation\OneBot\BotEvent::class, 'BotEvent');
 class_alias(\ZM\Annotation\OneBot\CommandArgument::class, 'CommandArgument');
+class_alias(\ZM\Annotation\OneBot\CommandHelp::class, 'CommandHelp');
+
 class_alias(\ZM\Annotation\Closed::class, 'Closed');
+
 class_alias(\ZM\Plugin\ZMPlugin::class, 'ZMPlugin');
-class_alias(\OneBot\V12\Object\OneBotEvent::class, 'OneBotEvent');
 class_alias(\ZM\Context\BotContext::class, 'BotContext');
 class_alias(\ZM\Store\KV\LightCache::class, 'LightCache');
 class_alias(\ZM\Store\KV\Redis\KVRedis::class, 'KVRedis');
@@ -23,3 +31,5 @@ class_alias(\OneBot\Driver\Event\WebSocket\WebSocketOpenEvent::class, 'WebSocket
 class_alias(\OneBot\Driver\Event\WebSocket\WebSocketCloseEvent::class, 'WebSocketCloseEvent');
 class_alias(\OneBot\Driver\Event\WebSocket\WebSocketMessageEvent::class, 'WebSocketMessageEvent');
 class_alias(\OneBot\Driver\Event\Http\HttpRequestEvent::class, 'HttpRequestEvent');
+
+class_alias(\OneBot\V12\Object\OneBotEvent::class, 'OneBotEvent');
