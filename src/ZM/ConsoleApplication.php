29d7d07c8eb5d824640128e573678bf515935d5d
sunxyw <git@sunxyw.xyz>
2023-01-26 21:59:27 +0800

    increment build number (build 480)

--- src/ZM/ConsoleApplication.php
+++ src/ZM/ConsoleApplication.php
@@ -28,9 +28,9 @@ use ZM\Exception\InitException;
 
 class ConsoleApplication extends Application
 {
-    public const VERSION_ID = 479;
+    public const VERSION_ID = 480;
 
-    public const VERSION = '2.8.5';
+    public const VERSION = '2.8.6';
 
     private static $obj;
 
