73f08085182f2d7e24bdce2b3aa1a39577ab1b4f
Jerry <jesse2061@outlook.com>
2023-01-30 15:27:19 +0800

    cs fix

--- src/ZM/Command/Server/ServerStartCommand.php
+++ src/ZM/Command/Server/ServerStartCommand.php
@@ -9,7 +9,6 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use ZM\Exception\InitException;
 use ZM\Exception\ZMKnownException;
 use ZM\Framework;
 use ZM\Process\ProcessStateManager;
