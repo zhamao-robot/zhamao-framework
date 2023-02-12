4cf314b70106415a6f26e165ffc1ac49ef003ce5
crazywhalecc <crazywhalecc@users.noreply.github.com>
2023-02-12 16:33:22 +0000

    increment build number (build 693)

--- src/ZM/Framework.php
+++ src/ZM/Framework.php
@@ -44,7 +44,7 @@ class Framework
     use Singleton;
 
     /** @var int 版本ID */
-    public const VERSION_ID = 692;
+    public const VERSION_ID = 693;
 
     /** @var string 版本名称 */
     public const VERSION = '3.0.3';
