7c863ba2c7e515e1143a0155a3bfa9a60646e32e
Fish <65108397+FishZe@users.noreply.github.com>
2023-02-13 01:33:41 +0800

    add what2eat plugin to plugin market (#309)

--- docs/.vuepress/components/plugin_list.js
+++ docs/.vuepress/components/plugin_list.js
@@ -10,5 +10,11 @@ module.exports = [
         description: '框架接入 gocq 的适配器（将 OneBot 11 转为 12）',
         avatar: 'https://user-images.githubusercontent.com/25968335/120111974-8abef880-c139-11eb-99cd-fa928348b198.png',
         link: 'https://github.com/zhamao-robot/go-cqhttp-adapter-plugin'
+    },
+    {
+        name: 'what2eat',
+        description: '今天吃什么',
+        avatar: 'null',
+        link: 'https://github.com/FishZe/zhamao_plugin_what2eat'
     }
 ];
