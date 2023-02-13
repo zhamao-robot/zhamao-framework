05619f0ae902694d23bbbf863f7062d9154ea8a6
AeQiQi <59356882+AeQiQi@users.noreply.github.com>
2023-02-13 19:23:48 +0800

    Update plugin_list.js

--- docs/.vuepress/components/plugin_list.js
+++ docs/.vuepress/components/plugin_list.js
@@ -16,5 +16,11 @@ module.exports = [
         description: '今天吃什么',
         avatar: 'null',
         link: 'https://github.com/FishZe/zhamao_plugin_what2eat'
+    },
+    {
+        name: 'plugin-handle',
+        description: '炸毛框架的猜成语插件，仅适配 onebots 实现。',
+        avatar: 'https://portrait.gitee.com/uploads/avatars/user/1726/5178850_Liufeiii_1609642372.png',
+        link: 'https://gitee.com/liufeii/plugin-handle'
     }
 ];
