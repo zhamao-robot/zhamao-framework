f7bebbdb9e050bd3923bf760e58b202ddb433054
crazywhalecc <jesse2061@outlook.com>
2023-03-10 21:43:26 +0800

    fix BotActionResponse status filter

--- src/ZM/Framework.php
+++ src/ZM/Framework.php
@@ -50,7 +50,7 @@ class Framework
     public const VERSION_ID = 704;
 
     /** @var string 版本名称 */
-    public const VERSION = '3.1.1';
+    public const VERSION = '3.1.2';
 
     /**
      * @var RuntimePreferences 运行时偏好（环境信息&参数）
