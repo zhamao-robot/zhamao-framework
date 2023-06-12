455e09c66d18e3f7518fcea8d903de74d86d66de
crazywhalecc <jesse2061@outlook.com>
2023-06-12 18:15:00 +0800

    update to 3.2.3, fix ergodic annotation order

--- src/ZM/Framework.php
+++ src/ZM/Framework.php
@@ -50,7 +50,7 @@ class Framework
     public const VERSION_ID = 722;
 
     /** @var string 版本名称 */
-    public const VERSION = '3.2.2';
+    public const VERSION = '3.2.3';
 
     /**
      * @var RuntimePreferences 运行时偏好（环境信息&参数）
