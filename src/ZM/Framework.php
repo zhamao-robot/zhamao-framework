f3eb9ccc817316fc78895e190c202beb460a4c08
Jerry <jesse2061@outlook.com>
2023-06-13 09:23:34 +0800

    fix class empty caused bug

--- src/ZM/Framework.php
+++ src/ZM/Framework.php
@@ -50,7 +50,7 @@ class Framework
     public const VERSION_ID = 723;
 
     /** @var string 版本名称 */
-    public const VERSION = '3.2.3';
+    public const VERSION = '3.2.4';
 
     /**
      * @var RuntimePreferences 运行时偏好（环境信息&参数）
